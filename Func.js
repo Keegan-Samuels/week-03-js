38724943af78744280e81b8de7b4372cf64f5e03
Keegan Samuels <keegansamuel19@gmail.com>
2025-06-02 13:57:26 +0200

    Add files via upload

---
+++ Func.js
@@ -0,0 +1,19 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name) {
+    // TODO: Make the greet function return "Hello, [name]!"
+    return `Hello, ${name}!`;
+}
+
+// TODO: Create a function called add that takes two parameters
+function add(num1, num2) {
+    // TODO: Make the add function return the sum of both parameters
+    return num1 + num2;
+}
+
+// TODO: Call both functions and store their results in variables
+const greetingResult = greet("Aliyah");
+const sumResult = add(5, 7);
+
+// TODO: Print the results to the console
+console.log(greetingResult);
+console.log(sumResult);
